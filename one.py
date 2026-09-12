9c9de20ab44ce2796584c6caa8219a143cf494a8
Rakesh Reddy <rakeshreddybuilds@gmail.com>
2026-09-12 11:43:34 +0530

    Updated skill

--- one.py
+++ one.py
@@ -1,2 +1,9 @@
 print("Hello rakesh")
 print("lets start this winter arc")
+
+print("Let's make this arc a comeback that no one would ever imagine")
+
+print("Focus on these three things in life")
+print("1.Beliefs changing the beliefs you have about yourself")
+print("2.Skillset Increasing the skills and building the proof")
+print("3.Character Traits Having high standards")
