36ec59fe15265e87ed9fe1d43932504a778a5be5
Rakesh Reddy <rakeshreddybuilds@gmail.com>
2026-09-14 10:03:22 +0530

    Learning new concepts in python

---
+++ lists.py
@@ -0,0 +1,51 @@
+# names = ["rakesh", "ben", "asta", "luffy", "zoro"]
+
+# print(names)
+
+
+# numbers = [2,4 ,8,10]
+
+# print(max(numbers))
+
+# numbers = [2,4 ,8,10]
+# max = numbers[0]
+
+# for number in numbers:
+#   if number > max:
+#     max = number
+# print(max)
+
+#2D lists
+# matrix = [
+#   [1,3,3],
+#   [1,8,2],
+#   [5,6,7]
+# ]
+
+# print(matrix)
+# print(matrix[0][2])
+# print(matrix)
+# for row in matrix:
+#   for item in row:
+#     print(item)
+
+
+# List methods
+# numbers = [2,4 ,9,20,8,10]
+# numbers.append(20)
+# numbers.insert(0,4)
+# numbers.clear()
+# print(50 in numbers)
+# numbers.sort()
+# # numbers.copy()
+# print(numbers)
+
+# numbers = [2, 2, 4 , 5, 6, 4, 6, 20]
+
+# uniques = []
+# for number in numbers:
+#   if number not in uniques:
+#     uniques.append(number)
+# print(uniques)
+
+
